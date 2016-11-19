14f2a91f3e8cae7e35cabf75dc568e3283a6f8af
JohnStranahan <jasballpythons@msn.com>
2016-11-19 14:58:18 -0500

    Add files via upload

---
+++ src/AmazonSwordPlant.java
@@ -0,0 +1,27 @@
+package aquarium;
+
+public class AmazonSwordPlant extends Plants
+{
+
+	public AmazonSwordPlant() {
+		super();
+		super.setName("Amazon Sword Plant");
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public static void main(String[] args) 
+	
+	{
+		System.out.println(getName());
+		
+	}
+	
+		
+	}
+
+	
+	
+	
+	
+
---
+++ src/driver.java
@@ -0,0 +1,10 @@
+package aquarium;
+
+public class driver {
+
+	public static void main(String[] args) {
+		AmazonSwordPlant amazon = new AmazonSwordPlant();
+		System.out.println(amazon.getName());
+	}
+
+}
